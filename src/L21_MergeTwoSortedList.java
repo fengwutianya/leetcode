52e0d5a14f1271f97623f54d6b7a14eaec116dbd
fengwutianya <949979029@qq.com>
2016-10-26 10:43:38 +0800

    L21 recursive solution

--- src/L21_MergeTwoSortedList.java
+++ src/L21_MergeTwoSortedList.java
@@ -8,20 +8,33 @@ public class L21_MergeTwoSortedList {
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
                 now.next = l1;
-                now = now.next;
                 l1 = l1.next;
             } else {
                 now.next = l2;
-                now = now.next;
                 l2 = l2.next;
             }
+            now = now.next;
         }
-        l1 = (l1 != null ? l1 : l2);
-        while (l1 != null) {
-            now.next = l1;
-            l1 = null;
-        }
+        now.next = (l1 != null ? l1 : l2);
 
         return l.next;
+    }
+    /**
+     * recursive solution
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode pHead = null;
+        if (l1.val < l2.val) {
+            pHead = l1;
+            pHead.next = mergeTwoLists(l1.next, l2);
+        } else {
+            pHead = l2;
+            pHead.next = mergeTwoLists(l1, l2.next);
+        }
+
+        return pHead;
     }
 }
